5576ba1828e1fd1434998e52df2e9e0675449ef0
Arnold Vink <info@arnoldvink.com>
2020-09-25 12:14:53 +0200

    - Added scroll to last unread item function.

--- NewsScroll/Events/EventsItemStatus.cs
+++ NewsScroll/Events/EventsItemStatus.cs
@@ -1,7 +1,11 @@
 ﻿using ArnoldVinkMessageBox;
+using NewsScroll.Classes;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace NewsScroll
@@ -18,11 +22,11 @@ namespace NewsScroll
                     string ReturnToPrevious = String.Empty;
                     if (PreviousItemId != 0 && TargetListViewItems > PreviousItemId) { ReturnToPrevious = "Scroll back to " + PreviousItemId; }
 
-                    Int32 MsgBoxResult = await AVMessageBox.Popup("Item scroller", "Would you like to scroll in the items?", "Scroll to beginning", "Scroll to the middle", "Scroll to the end", ReturnToPrevious, "", true);
+                    Int32 MsgBoxResult = await AVMessageBox.Popup("Item scroller", "Would you like to scroll in the items?", "Scroll to beginning", "Scroll to the middle", "Scroll to the end", "Scroll to unread item", ReturnToPrevious, true);
                     if (MsgBoxResult == 1)
                     {
                         await Task.Delay(10);
-                        TargetListView.ScrollIntoView(TargetListView.Items.First());
+                        TargetListView.ScrollIntoView(TargetListView.Items.FirstOrDefault());
                         return true;
                     }
                     else if (MsgBoxResult == 2)
@@ -34,10 +38,25 @@ namespace NewsScroll
                     else if (MsgBoxResult == 3)
                     {
                         await Task.Delay(10);
-                        TargetListView.ScrollIntoView(TargetListView.Items.Last());
+                        TargetListView.ScrollIntoView(TargetListView.Items.LastOrDefault());
                         return true;
                     }
                     else if (MsgBoxResult == 4)
+                    {
+                        await Task.Delay(10);
+                        IEnumerable<Items> itemsList = TargetListView.Items.OfType<Items>();
+                        Items scrollItem = itemsList.Where(x => x.item_read_status == Visibility.Collapsed).FirstOrDefault();
+                        if (scrollItem != null)
+                        {
+                            TargetListView.ScrollIntoView(scrollItem);
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if (MsgBoxResult == 5)
                     {
                         await Task.Delay(10);
                         TargetListView.ScrollIntoView(TargetListView.Items[PreviousItemId]);
